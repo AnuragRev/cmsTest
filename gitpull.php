f206ff25742d65f9049ea76dbef4f7c55d8d61dc
AnuragRev <anurag@revinfotech.com>
2015-09-04 11:39:51 +0530

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -1,13 +1,1 @@
-<?php 
-
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
-
-$output = shell_exec("sudo git pull https://github.com/AnuragRev/cmsTest.git");
-
-echo "<pre>";
-print_r($output);
-echo "</pre>";
-
-?>
+<?php `sudo git pull https://github.com/AnuragRev/cmsTest.git`; ?>
